05501d4bd7aa2dcca2b642a9200479cdc20cb285
Anil kumar <59431789+anilkumar541@users.noreply.github.com>
2023-06-09 11:45:16 +0530

    first commit

---
+++ frontend_reactjs/src/pages/Contact.js
@@ -0,0 +1,15 @@
+import { Grid } from "@mui/material";
+
+const Contact = () => {
+  return <>
+    <Grid container justifyContent='center'>
+      <Grid item sm={10}>
+        <h1>Contact Us</h1>
+        <hr />
+        <p></p>
+      </Grid>
+    </Grid>
+  </>;
+};
+
+export default Contact;
---
+++ frontend_reactjs/src/pages/Home.js
@@ -0,0 +1,15 @@
+import { Grid } from "@mui/material";
+
+const Home = () => {
+  return <>
+    <Grid container justifyContent='center'>
+      <Grid item sm={10}>
+        <h1> Welcome to the Oscorp</h1>
+        <hr />
+        <p>Oscorp, also known as Oscorp Industries, is a fictional multibillion-dollar multinational corporation appearing in American comic books published by Marvel Comics, predominantly in stories about Spider-Man. The company was founded by Norman Osborn and has appeared in numerous media adaptations.</p>
+      </Grid>
+    </Grid>
+  </>;
+};
+
+export default Home;
